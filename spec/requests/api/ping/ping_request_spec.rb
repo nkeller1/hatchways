31eed02a208ac89f7c1b1badab53799462542164
Nathan Keller <keller.nathan@gmail.com>
2020-08-06 11:46:53 -0600

    Add additonal test for ping route.

--- spec/requests/api/ping/ping_request_spec.rb
+++ spec/requests/api/ping/ping_request_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Ping the Hatchways API' do
     get '/api/ping'
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
     expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
     expect(response.body).to include("{\"success\":true}")
   end
