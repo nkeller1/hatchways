220d1d1fe9c7969f15600938ed2611cca767b099
Nathan Keller <keller.nathan@gmail.com>
2020-08-06 13:26:01 -0600

    Response successfully accounts for sortBy and direction.

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -4,17 +4,24 @@ class Api::PostController < ApplicationController
       url: 'https://hatchways.io'
     )
 
+    # tags = params['tag'].split(',')
     tag = params['tag']
     sortBy = params['sortBy']
     direction = params['direction']
 
     response = connection.get('/api/assessment/blog/posts') do |req|
       req.params['tag'] = tag
-      req.params['sortBy'] = sortBy
-      req.params['diretion'] = direction
     end
 
-    # require "pry"; binding.pry
-    render json: response.body
+    response = JSON.parse(response.body)['posts']
+      ordered_response = response.sort_by do |post|
+        post[sortBy]
+      end
+
+    if direction == 'desc'
+      ordered_response = ordered_response.reverse!
+    end
+    require "pry"; binding.pry
+    render json: ordered_response
   end
 end
--- spec/requests/api/posts/post_request_spec.rb
+++ spec/requests/api/posts/post_request_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe '/post route' do
-  it "recieves a a successful response with a single tag param", :vcr do
+  # it "recieves a a successful response with a single tag param", :vcr do
+  #
+  #   get '/api/posts?tag=tech'
+  #
+  #   expect(response).to be_successful
+  #   expect(response.status).to eq(200)
+  #   expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+  # end
 
-    get '/api/posts?tag=tech'
+  # it "recieves a a successful response with a two tags", :vcr do
+  #
+  #   get '/api/posts?tag=tech,science'
+  #
+  #   expect(response).to be_successful
+  #   expect(response.status).to eq(200)
+  #   expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+  # end
+
+  # it "recieves a a successful response with sortBy and direction=asc query Params", :vcr do
+  #
+  #   get '/api/posts/?tag=tech&sortBy=likes&direction=asc'
+  #
+  #   expect(response).to be_successful
+  #   expect(response.status).to eq(200)
+  #   expect(response.headers["Content-Type"]).to eq("application/json; charset=utf-8")
+  # end
+
+  it "recieves a a successful response with sortBy and direction=desc query Params", :vcr do
+
+    get '/api/posts/?tag=tech&sortBy=likes&direction=desc'
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
